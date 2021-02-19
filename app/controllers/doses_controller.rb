449adfee5e0cc26a8a3a21471345c460dccfc257
Felicia Goh <minhui.felicia@gmail.com>
2021-02-19 14:52:56 +0800

    Add delete dose

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,7 @@
 class DosesController < ApplicationController
   def index
-    @doses = Dose.all
-  end
-
-  def show
-    @dose = Dose.find(params[:id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @doses = Dose.where(cocktail: @cocktail)
   end
 
   def new
@@ -25,6 +22,9 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_doses_path(@dose.cocktail)
   end
 
   private
